fc8e2f2b14bda3c3767bdb8f6e170ce29b4089e7
maxonss <hello@maxencepuhetini.com>
2021-02-27 18:34:52 +0100

    Added mezon/router again ;)

--- index.php
+++ index.php
@@ -5,41 +5,15 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // Load the autoloader
 require_once('core/autoloader.php');
+require_once('vendor/autoload.php');
 // Initialize the connection to the database
 Parameter::initialization();
 Connect::initialization();
-
-/**
- * Loads the needed page
- * @param array $p tableau associatif contenant les informations relatives à la page
- */
-// function pageLoader($p, $debug = false, $debug_routes = null)
-// {
-//     $path  =  $p[0];
-//     $name  =  $p[1];
-//     $title =  $p[2];
-
-//     include "view/head.php";
-//     include "view/header.php";
-//     include "view$path/$name.php";
-//     include "view/footer.php";   
-// }
-
-// $routes = [
-//     '/'       => ['', 'homepage', 'Welcome'],
-//     'default' => ['', 'homepage', 'Welcome'],
-//     '404'     => ['', '404-not-found', 'Oups'],
-//     'users'   => ['', 'list', 'User list'],
-//     'posts'   => ['', 'posts', 'Posts list']
-// ];
-
-// if (isset($_GET, $_GET['p'])) {
-//     $p = $_GET['p'];
-//     if (isset($routes[$p])) {
-//         pageLoader($routes[$p]);
-//     } else {
-//         pageLoader($routes['default']);
-//     }
-// } else {
-//     pageLoader($routes['default']);
-// }
+$router = new \Mezon\Router\Router();
+Route::initialization($router);
+// Loading every needed HTML structure files
+require('view/head.php');
+require('view/header.php');
+$router->callRoute($_SERVER['REQUEST_URI']);
+// Load the rest of needed HTML files
+require('view/footer.php');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '93e8eda8aebe62673a96a081c248a6832e092cf2',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '93e8eda8aebe62673a96a081c248a6832e092cf2',
+    ),
+    'mezon/router' => 
+    array (
+      'pretty_version' => '1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '56b816e38b0ae060a1884ab95ecda781212a4cba',
+    ),
+  ),
+);
